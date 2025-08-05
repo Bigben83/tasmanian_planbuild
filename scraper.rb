45b5fbbb0145273fc0f40a0134878c89e008ab00
Benjamin Harris <Bigben83@users.noreply.github.com>
2025-08-05 14:07:14 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,7 @@
-require 'net/http'
-require 'json'
+require 'mechanize'
 require 'sqlite3'
+require 'json'
 require 'logger'
-require 'uri'
 
 logger = Logger.new(STDOUT)
 db = SQLite3::Database.new "data.sqlite"
@@ -17,41 +16,31 @@ db.execute <<-SQL
   );
 SQL
 
-# Define the API endpoint and payload
-uri = URI('https://portal.planbuild.tas.gov.au/api/advertisement/search')
-headers = {
-  'Content-Type' => 'application/json',
-  'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-  'Origin' => 'https://portal.planbuild.tas.gov.au',
-  'Referer' => 'https://portal.planbuild.tas.gov.au/external/advertisement/search',
-  'Accept' => 'application/json, text/plain, */*'
-}
+agent = Mechanize.new
+agent.user_agent_alias = 'Windows Chrome'
 
-# Example LGA code (you'll want to loop through others)
-lga_code = 'LGA003'  # Replace with actual code for a council
+# Visit the initial page to get cookies/session
+logger.info("Visiting initial page to get session")
+agent.get("https://portal.planbuild.tas.gov.au/external/advertisement/search")
 
+# Now post to the JSON API
+uri = 'https://portal.planbuild.tas.gov.au/api/advertisement/search'
 payload = {
   advertisementType: 'ALL',
-  lgaCode: lga_code,
+  lgaCode: 'LGA003',
   offset: 0,
   pageSize: 100,
   sortField: 'advertisedDate',
   sortDirection: 'DESC'
 }
 
-# Make the request
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-request = Net::HTTP::Post.new(uri.request_uri, headers)
-request.body = payload.to_json
+logger.info("Posting to API endpoint")
+response = agent.post(uri, payload.to_json, {
+  'Content-Type' => 'application/json',
+  'Accept' => 'application/json'
+})
 
-logger.info("Sending request to API for LGA code: #{lga_code}")
-response = http.request(request)
-
-logger.info("Response code: #{response.code}")
-logger.debug("Response body: #{response.body}")
-
-if response.code.to_i == 200
+if response.code == '200'
   results = JSON.parse(response.body)
   results['data'].each do |item|
     address = item['address']
@@ -59,10 +48,10 @@ if response.code.to_i == 200
     date = item['advertisedDate']
 
     logger.info("Saving: #{address} | #{reference} | #{date}")
-
     db.execute("INSERT INTO advertisement_data (address, council_reference, advertised_date)
                 VALUES (?, ?, ?)", [address, reference, date])
   end
 else
   logger.error("API call failed with status #{response.code}")
+  logger.debug("Body: #{response.body}")
 end
